e4961eecb6d84f4ab927c9626444dfbd944f8c4a
John Tuomi III <jtuomi2@gmail.com>
2015-05-06 18:29:06 -0500

    Created Markdown Blog Rendering Function
    Allows for creation of blogs in markdown format that gets rendered as
    HTML5. A list is created on the landing page that lists all .md files.

--- site_project291/site_project291/routes.py
+++ site_project291/site_project291/routes.py
@@ -39,23 +39,38 @@ def about():
 @route('/blog')
 @view('blog')
 def blog():
-    md = markdown.Markdown(output_format='html5')
+    content = ''
+    last_10_blogs = []
     """Renders the blog page."""
     for root, dirs, files in os.walk('static/blog'):
         for file in files:
             if file.endswith(".md"):
+                last_10_blogs.append(str("<a href='%s'>%s</a>" % (os.path.join(root, file).replace('//','\\'),file)))
+    return dict(
+                title='About',
+                message='Your application description page.',
+                year=datetime.now().year,
+                blog_content= content,
+                last_10 = last_10_blogs
+                )
+    
+@route('/static/blog/<filename>')
+@view('blog')
+def blogArticle(filename):
+    last_10_blogs = []
+    print(filename)
+    md = markdown.Markdown(output_format='html5')
+    for root, dirs, files in os.walk('static/blog'):
+        for file in files:
+            if file == filename:
                 f = open(os.path.join(root, file).replace('//','\\'),'r')
                 content = f.read()
                 content = md.convert(content)
-
-                
-                
-        
-    
     return dict(
-        title='About',
-        message='Your application description page.',
-        year=datetime.now().year,
-        blog_content= content
-    )
+                title=str(filename).replace('.md',''),
+                message='Your application description page.',
+                year=datetime.now().year,
+                blog_content= content,
+                last_10 = []
+                )
     
--- site_project291/site_project291/views/blog.tpl
+++ site_project291/site_project291/views/blog.tpl
@@ -1,5 +1,14 @@
 % rebase('blog_layout.tpl', title=title, year=year)
 <div class="blog-container">
 	<div class="overlay"></div>
+	% if len(last_10) != 0:	
+		<ul>
+		% for link in last_10:
+		<li>{{ !link }} </li>
+		% end
+		</ul>
+	% end
+	% if blog_content != None or blog_content != '':
 	{{ !blog_content }}
+	%end
 </div>
